eb5b0294f8d0cac236cf51cf6032c87cdf848002
Marios Kogias <marioskogias@gmail.com>
2020-11-16 12:45:46 +0000

    Update pseudo api

--- design-docs/pseudo_api.py
+++ design-docs/pseudo_api.py
@@ -13,45 +13,45 @@ Opinions:
     - Read/Write operations have to be non blocking. We should expose their failures.
 
 Suggestion:
-    Each connection is 3 cowns (read_fd, write_fd, cmd_fd)
+    Each connection is 3 cowns (read_endpoint, write_endpoint, cmd_endpoint)
 """
 
-# Called with the write_fd acquired
+# Called with the write_endpoint acquired
 def write_rest(buf):
-    res = write(write_fd, buf, N)
+    res = write(write_endpoint, buf, N)
     if res < N:
-        schdule_when_write_available(write_rest, buf)
+        schdule(write_rest, (write_endpoint), buf)
         return
     else:
         # If used with readfn not readfn2
-        schedule(readfn, (read_fd, write_fd))
+        schedule(readfn, (read_endpoint, write_endpoint))
 
-# Called with read_fd and write_fd acquired
+# Called with read_endpoint and write_endpoint acquired
 def readfn():
     while True:
-        res = read(read_fd, buf, N)
+        res = read(read_endpoint, buf, N)
         if res == -1:
-            schedule_when_read_available(readfn, (read_fd, write_fd))
+            schedule(readfn, (read_endpoint, write_endpoint))
             break
 
-        res = write(write_fd, buf, N)
+        res = write(write_endpoint, buf, N)
         if res < N:
-            schdule_when_write_available(write_rest, (write_fd), buf)
+            schdule(write_rest, (write_endpoint), buf)
 
-# Alternative read function called with read_fd acquired only
+# Alternative read function called with read_endpoint acquired only
 def readfn2():
     while True:
-        res = read(read_fd, buf, N)
+        res = read(read_endpoint, buf, N)
         if res == -1:
-            schedule_when_read_available(readfn)
+            schedule(readfn, (read_endpoint))
             break
 
-        schedule(write_rest, buf)
+        schedule(write_rest, (write_endpoint), buf)
 
 
 def main():
-    read_fd, write_fd = TCPSocket()
-    schedule(readfn, (read_fd, write_fd))
+    read_endpoint, write_endpoint = TCPSocket()
+    schedule(readfn, (read_endpoint, write_endpoint))
 
     # Or alternatively
-    schedule(readfn2, (read_fd))
+    schedule(readfn2, (read_endpoint))
